643fd3ac9d5fdc55a6ed707bf8b059ffd0b057ab
cayden.ngai <cayden.ngai@lalamove.com>
2022-06-08 18:58:01 +0800

    add while loop and option menu

--- main.py
+++ main.py
@@ -27,37 +27,53 @@ def add_margin(pil_img):
     else:
         return pil_img
 
-
-def mainFunc():
-    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
-
-    ### Entire Directory
+def entire_directory():
     directory_path = askdirectory()
-    if(directory_path == ""):
+
+    if (directory_path == ""):
         print("not chosen")
         return
     chosen_directory = os.listdir(directory_path)
     for i in chosen_directory:
-        if(i.endswith('.DS_Store')):
+        if (i.endswith('.DS_Store')):
+            print("2")
             continue
         im = Image.open(directory_path + "/" + i)
         im_new = add_margin(im)
         output_path_and_filename = "./output/" + i
-        print(output_path_and_filename)
         im_new.save(output_path_and_filename, quality=100)
 
-    ### Single File
-    # file_path = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
-    # if(file_path == ""):
-    #     print("not chosen")
-    #     return
-    # im = Image.open(file_path)
-    # filename = get_filename(file_path)
-    # print("filename: " + filename)
-    # im_new = add_margin(im)
-    # output_path_and_filename = "./output/" + filename
-    # im_new.save(output_path_and_filename, quality=100)
+def single_file():
+
+    file_path = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    if(file_path == ""):
+        print("not chosen")
+        return
+    im = Image.open(file_path)
+    filename = get_filename(file_path)
+    im_new = add_margin(im)
+    output_path_and_filename = "./output/" + filename
+    im_new.save(output_path_and_filename, quality=100)
+
+
+def mainFunc():
+    option = ""
+    while(option != "3"):
+
+        option = input("Enter your option: \n 1 - Choosing Folder \n 2 - Choosing Single Photo \n 3 - Exit \n")
+        if(option == "3"):
+            print("See you~")
+            break
+
+        Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+        if(option == "1"):
+            entire_directory()
+            print("Done. Please check the output folder")
+        if(option == "2"):
+            single_file()
+            print("Done. Please check folder you chose")
 
 if __name__ == '__main__':
     mainFunc()
+
 
